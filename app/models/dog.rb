6c59066d50b6e404dbda76dbde9d13228c447750
jamieberrier <berrier.jamie@gmail.com>
2019-07-01 22:04:32 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,8 @@
+# This class should have name, breed, and age attributes which
+# can be set on initialization. You should be able to read and
+# write to these attributes. This class should also keep track
+# of each instance of dog created, as well as a class method all
+# to return an array of those instances.
 class Dog
-  
+
 end
