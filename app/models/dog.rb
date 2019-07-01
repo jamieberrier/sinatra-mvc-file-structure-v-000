c478a9c0694a7e37c09acd2c9e8dc8e788114f59
jamieberrier <berrier.jamie@gmail.com>
2019-07-01 22:08:50 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -8,11 +8,11 @@ class Dog
 
   @@all = []
 
-  def initialize(name, breed, age)
-    name = name
-    breed = breed
-    age = age
-    
+  def initialize(:name, :breed, :age)
+    name = :name
+    breed = :breed
+    age = :age
+
     @@all << self
   end
 
